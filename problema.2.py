fe69211bea35db29e4d5e63db7722ec2fed78f33
AndreiApopii <70907129+AndreiApopii@users.noreply.github.com>
2020-11-11 12:39:04 +0300

    Update problema.2.py

--- problema.2.py
+++ problema.2.py
@@ -1,9 +1,9 @@
 n=int(input('dati nr: '))
 s=0
-for y in range(1,n+1):
-    x=y
-    while (y-1)>0:
-        x*=y-1
-        y-=1
+for m in range(1,n+1):
+    x=m
+    while (m-1)>0:
+        x*=m-1
+        m-=1
     s+=x
 print('suma=',s)
